b9f08095c03f7951a926adc7cd31ad9c3bb5cdc3
Dmitrij Povasar <dmitrij.povasar@visma.com>
2020-10-26 16:21:47 +0200

    Badge icon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { SurvivorListComponent } from './survivor-list/survivor-list.component';
 import { TeamListComponent } from './team-list/team-list.component';
 
 const routes: Routes = [
+  { path: '', component: SurvivorListComponent },
   { path: 'survivor', component: SurvivorListComponent },
   { path: 'team', component: TeamListComponent }
 ];
